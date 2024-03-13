9b7f67092c329bb32f80061d1abdb1fb60c8c4ac
Nelius-Wandia <151659348+Nelius-Wandia@users.noreply.github.com>
2024-03-13 17:38:15 +0300

    added a while loop

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ guess_count = 0
 guess_limit = 3
 
 while guess_count < guess_limit:
-    
+    guess = int(input("Guess the number: "))
+    guess_count += 1
+    if guess == secret_number:
+        print("Congratulations! You won ") 
+        break
+else:
+    print("Sorry! You failed")
